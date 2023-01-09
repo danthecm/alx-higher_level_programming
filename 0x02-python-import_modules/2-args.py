f3fb604cb1b2ad01acc17376f8ae507ad3cf8a21
danthecm <etchiedanie1@gmail.com>
2023-01-09 22:10:54 +0100

    fix style task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     print(
             '{} arguments'.format(len(sys.argv) - 1),
             end=f"{'.' if len(sys.argv) < 2 else ':'}\n"
