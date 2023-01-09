0cedd5894399df653e9067b0ce1f42ccc6928209
danthecm <etchiedanie1@gmail.com>
2023-01-09 21:41:43 +0100

    fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,9 @@
 from add_0 import add
 
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(1, 2)))
+    try:
+        a = 1
+        b = 2
+        print("{:d} + {:d} = {:d}".format(a, b, add(1, 2)))
+    except Exception as e:
+        raise e
