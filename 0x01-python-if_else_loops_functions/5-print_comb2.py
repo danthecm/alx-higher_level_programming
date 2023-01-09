4a52c15d20fd603689a53f2a6b2a4c823c6c8d1e
danthecm <etchiedanie1@gmail.com>
2023-01-09 10:26:17 +0100

    fix task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for i in range(0, 100):
     print(
             "{}".format(i if i > 9 else f"0{i}"),
-            end="{}".format(", " if i < 99 else "")
+            end="{}".format(", " if i < 99 else "\n")
         )
