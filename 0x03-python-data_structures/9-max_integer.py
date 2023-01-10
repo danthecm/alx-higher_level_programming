8c6153bb2173d394e0db0083246fea7141dffe44
danthecm <etchiedanie1@gmail.com>
2023-01-10 22:53:00 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    max_value = 0
+    for integer in my_list:
+        if max_value < integer:
+            max_value = integer
+    return max_value
