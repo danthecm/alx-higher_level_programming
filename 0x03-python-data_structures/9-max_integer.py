eb552164c73763eec6d8734e4f44c4bd2ab016c1
danthecm <etchiedanie1@gmail.com>
2023-01-10 22:54:19 +0100

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,10 @@ def max_integer(my_list=[]):
         return None
     max_value = 0
     for integer in my_list:
+        try:
+            int(integer)
+        except Exception as e:
+            raise e
         if max_value < integer:
             max_value = integer
     return max_value
