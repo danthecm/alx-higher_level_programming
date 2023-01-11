e2bb1fd9edd14e1ee1571bff235e584885eb61fb
danthecm <etchiedaniel1@gmail.com>
2023-01-11 22:32:09 +0100

    0x04 task 9 fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = {}
-    for key, value in a_dictionary:
+    for key, value in a_dictionary.items():
         new_value = value * 2
         new_dict.update({key: new_value})
     return new_dict
