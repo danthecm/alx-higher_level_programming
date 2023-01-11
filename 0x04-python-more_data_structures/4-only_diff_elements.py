c620c56a69ef8f8b84b83d68f732bcdba3298d2e
danthecm <etchiedaniel1@gmail.com>
2023-01-11 21:57:12 +0100

    0x04 task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    common = []
-    for item in set_1:
-        if item not in set_2:
-            common.append(item)
-    return common
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
