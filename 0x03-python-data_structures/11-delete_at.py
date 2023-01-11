2ba1bf73031037ce3ccce6d69777b6a644f280be
danthecm <etchiedaniel1@gmail.com>
2023-01-11 11:08:53 +0100

    0x03 task 11 correction

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     try:
-        my_list = [x for index, x in enumerate(my_list) if index != idx]
+        list_index = [index for index, x in enumerate(my_list) if index == idx]
+        my_list.remove(my_list[list_index[0]])
     except IndexError:
         return my_list
     return my_list
