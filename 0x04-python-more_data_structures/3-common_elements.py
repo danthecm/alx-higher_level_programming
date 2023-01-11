91d7f8f5a23201e57fea403f6685da144b8fb56c
danthecm <etchiedaniel1@gmail.com>
2023-01-11 21:58:06 +0100

    0x04 task 3 optimize

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
-    for item in set_1:
-        if item in set_2:
-            common.append(item)
-    return common
+    return set_1 & set_2
