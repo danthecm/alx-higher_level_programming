f9c7af780df78392eb08711848a09f0307aab5f9
danthecm <etchiedaniel1@gmail.com>
2023-01-11 21:47:13 +0100

    0x04 task 2 fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,3 @@ def uniq_add(my_list=[]):
         if x not in new_list:
             new_list.append(x)
     return sum(new_list)
-
-my_list = [1, 1, 1, 1, 1]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
