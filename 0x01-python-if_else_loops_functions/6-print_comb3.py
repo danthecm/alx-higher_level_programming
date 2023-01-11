6afc3dbd44d13d05c993b2567ea5653ba7de75ea
danthecm <etchiedaniel1@gmail.com>
2023-01-11 10:07:17 +0100

    0x01 task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,10 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i == j:
             continue
-        print("{}{}".format(i, j), end=", ")
+        if int(f"{i}{j}") > 9:
+            if i > j:
+                continue
+        if f"{i}{j}" != "89":
+            print("{}{}".format(i, j), end=", ")
+        else:
+            print("{}{}".format(i, j), end="\n")
