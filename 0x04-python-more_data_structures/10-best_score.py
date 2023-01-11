281c3992a29878d784da0dc7c8b327ce750d8d7e
danthecm <etchiedaniel1@gmail.com>
2023-01-11 22:43:17 +0100

    0x04 task 10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    best_score = max(a_dictionary.values())
-    return (list(a_dictionary.keys())[list(a_dictionary.values()).index(best_score)])
+    best_score = max(list(a_dictionary.values()))
+    return list(a_dictionary.keys())[list(a_dictionary.values()).index(best_score)]
