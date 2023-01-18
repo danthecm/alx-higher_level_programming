f33fc4d2673ec81951cca22970766dd3c87e3d75
CM <52335952+danthecm@users.noreply.github.com>
2023-01-18 23:03:22 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ import sys
 
 if __name__ == '__main__':
     arg_len = len(sys.argv) - 1
-    print('{} {}'.format(arg_len,"arguments" if arg_len != 1 else "argument"), end="")
+    print('{} {}'.format(
+        arg_len,
+        "arguments" if arg_len != 1 else "argument"
+        ),
+        end="")
     print("{}".format('.' if arg_len < 1 else ':'))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
