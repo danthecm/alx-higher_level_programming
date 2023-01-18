e61e5a2c06ae238a2c76c00e77e364428781e907
danthecm <etchiedaniel1@gmail.com>
2023-01-18 23:20:00 +0100

    fix 0x02 task 3

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str * 3}")
-print(f"{str[:9]}")
+print("{}".format(str * 3))
+print("{}".format(str[:9]))
--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-school = f"{str1} {str2}"
-print(f"Welcome to {school}!")
+school = str1 + " " + str2
+print("Welcome to {}!".format(school))
