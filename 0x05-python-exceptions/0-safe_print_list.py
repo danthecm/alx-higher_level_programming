128613f8577dc09cbcc57028ffde56275eb2acc0
danthecm <etchiedaniel1@gmail.com>
2023-01-23 21:24:46 +0100

    0x05 task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+ #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    number = 0
+    try:
+        for i in range(0, x+1):
+            number += 1
+            print(my_list[i], end="")
+    except:
+        pass
+    return number
