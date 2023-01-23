32b356637e63ba2fda5abb0362c75c002f456621
danthecm <etchiedaniel1@gmail.com>
2023-01-23 22:03:35 +0100

    0x05 task 2 fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     real_number = 0
-    try:
-        for i in range(0, x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                real_number += 1
-            except (ValueError, Exception):
-                continue
-        print("\n", end="")
-    except IndexError as e:
-        raise e
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            real_number += 1
+        except (ValueError, Exception) as e:
+            if not isinstance(e, ValueError):
+                raise e
+            pass
+    print("\n", end="")
     return real_number
