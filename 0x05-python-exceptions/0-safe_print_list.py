1284d7df1be468093d4278c4e653397170dc3a91
danthecm <etchiedaniel1@gmail.com>
2023-01-23 21:40:14 +0100

    0x05 task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             number += 1
             print("{}".format(my_list[i]), end="") if i - 1 != x else print("{}".format(my_list[i]))
-    except:
+    except Exception as e:
         number = 0
     return number
