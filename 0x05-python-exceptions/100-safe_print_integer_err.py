8b74f7bc6555ebf23b2c6bc6195b5989698da658
danthecm <etchiedaniel1@gmail.com>
2023-01-23 22:40:36 +0100

    0x05 task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,4 +7,4 @@ def safe_print_integer_err(value):
         return True
     except Exception as e:
         print("Exception: {}".format(e),  file=sys.stderr)
-        return False
+    return False
