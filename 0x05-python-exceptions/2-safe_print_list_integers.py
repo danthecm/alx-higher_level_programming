a6f8a86830e7d9fac3c6f71c3c72da0d767cfd5b
danthecm <etchiedaniel1@gmail.com>
2023-01-23 21:59:59 +0100

    0x05 task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 real_number += 1
-            except ValueError:
+            except (ValueError, Exception):
                 continue
         print("\n", end="")
     except IndexError as e:
