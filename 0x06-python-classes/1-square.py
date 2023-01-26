38ba85432c47236e88c8ce831c2fbf467ebe5339
danthecm <etchiedaniel1@gmail.com>
2023-01-26 13:25:26 +0100

    0x06 task 1 first attempt

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A simple implementtation of a square class that accepts size
+"""
+
+
+class Square:
+    """A class for generating squares
+
+    Args:
+    sizes (int): The size of the square
+    """
+
+    def __init__(self, size) -> None:
+        self._size = size
