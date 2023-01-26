a34a281bcaae34d9d4a0773b7f4ce31eca6e131c
danthecm <etchiedaniel1@gmail.com>
2023-01-26 14:07:47 +0100

    0x06 task 4 fifth attempt

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,6 +24,10 @@ class Square:
     
     @size.setter
     def size(self, value) -> int:
+        if not isinstance(value, (int)):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
