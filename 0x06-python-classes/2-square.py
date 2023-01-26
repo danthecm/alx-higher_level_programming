59835cc3c3819bdf16943fe13bd8c623754dd164
danthecm <etchiedaniel1@gmail.com>
2023-01-26 13:45:35 +0100

    0x06 task 2 first attempt

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A simple implementtation of a square class that accepts size
+"""
+
+
+class Square:
+    """A class for generating squares
+
+    Args:
+    sizes (int): The size of the square
+    """
+
+    def __init__(self, size=0) -> None:
+        if not isinstance(size, (int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
