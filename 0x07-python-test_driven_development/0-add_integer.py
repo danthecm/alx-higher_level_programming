b46f4892565aab8f09c13367acf570b4fbaf5fb3
danthecm <etchiedaniel1@gmail.com>
2023-01-31 23:28:24 +0100

    1: 0x07 task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A module containing functions for adding two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns the result of adding two integers together
+
+    Args:
+    a: integer
+    b (optional): integer
+    """
+    if type(a) != int:
+        if type(a) != float:
+            raise TypeError("a must be an integer")
+        a = int(a)
+    elif type(b) != int:
+        if type(b) != float:
+            raise TypeError("b must be an integer")
+        b = int(b)
+    return a + b
