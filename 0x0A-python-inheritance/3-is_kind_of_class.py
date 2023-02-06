ac89f2c68185f46a2781ad9a1c98be807f9568d1
danthecm <etchiedaniel1@gmail.com>
2023-02-06 23:20:24 +0100

    1: 0x0A task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module containing a function to check if two instance
+are exactly of the same class, or inherits from thesame class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    A function to check if two instances are of the same class
+
+    Args:
+    obj: instance of class
+    a_class: name of class
+    """
+    return isinstance(obj, a_class) or issubclass(obj, a_class)
