06ec64a35ddebe9b61b3904ec377feece2050494
danthecm <etchiedaniel1@gmail.com>
2023-02-06 23:39:38 +0100

    1: 0x0A task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module containing a function to check if two instance
+are exactly of the same class, or inherits from thesame class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    A function to check if two instances are of the same class
+
+    Args:
+    obj: instance of class
+    a_class: name of class
+    """
+    return issubclass(obj.__class__, a_class) if obj.__class__ is not a_class else False
