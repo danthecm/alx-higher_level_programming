0bfa04ab22a80d03d1dceb880bfce1dd9204e755
danthecm <etchiedaniel1@gmail.com>
2023-02-07 00:50:39 +0100

    2: 0x0A task 12

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,14 +10,12 @@ class Square(BaseGeometry):
     A Rectangle class based on the base geometry class
     """
 
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
 
     def area(self):
-        return self.__width * self.__height
+        return self.__size * self.__size
