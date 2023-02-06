b8b838c9985a4ea6b69237c41d4548df9e400117
danthecm <etchiedaniel1@gmail.com>
2023-02-06 23:59:51 +0100

    1: 0x0A task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,24 +19,3 @@ class BaseGeometry:
             raise TypeError("<name> must be an integer")
         elif value <= 0:
             raise ValueError("<name> must be greater than 0")
-
-
-bg = BaseGeometry()
-
-# bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
-
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
