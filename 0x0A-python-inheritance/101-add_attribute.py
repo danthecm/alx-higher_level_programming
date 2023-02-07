bd99028b5078db1c8aa4af661242a4a16a1b338a
danthecm <etchiedaniel1@gmail.com>
2023-02-07 01:18:17 +0100

    5: 0x0A task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -18,14 +18,6 @@ def add_attribute(obj, attr_name, attr_value):
     """
     if not hasattr(obj, '__setattr__'):
         raise TypeError("can't add new attribute")
-    elif isinstance(obj.__class__, (int, float, bool, str)):
+    elif isinstance(obj, (int, float, bool, str)):
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
-
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
