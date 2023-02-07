338f840823a033c66f76074a6bf4e236501feaae
danthecm <etchiedaniel1@gmail.com>
2023-02-07 15:47:44 +0100

    2: 0x0B task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,13 @@ and prints it to stdout
 
 
 def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout
+
+    Args:
+    filename (str): The filename
+    """
     if filename != "":
-        with open(filename, "r", "utf-8") as f:
+        with open(filename, "r", encoding="utf-8") as f:
             for line in f:
                 print(line, end="")
