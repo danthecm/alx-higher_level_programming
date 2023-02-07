edd0441c99db287cfaec8561a178bdb1a1aeb77b
danthecm <etchiedaniel1@gmail.com>
2023-02-07 01:06:37 +0100

    2: 0x0A task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,14 +5,20 @@ A module containing a class that Myint
 
 
 class MyInt(int):
-    """MyInt is a rebel class that inherits from int. 
+    """MyInt is a rebel class that inherits from int.
     The == and != operators are inverted in this class.
     """
 
     def __eq__(self, other):
-        """Return the opposite of the result of the equality comparison of self and other."""
+        """
+        Return the opposite of the result of the
+        equality comparison of self and other.
+        """
         return not super().__eq__(other)
 
     def __ne__(self, other):
-        """Return the opposite of the result of the inequality comparison of self and other."""
+        """
+        Return the opposite of the result of the
+        inequality comparison of self and other.
+        """
         return not super().__ne__(other)
