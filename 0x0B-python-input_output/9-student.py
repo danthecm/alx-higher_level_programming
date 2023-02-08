fec5db167850e29d35bd8ed0c59254cf4f0710cf
danthecm <etchiedaniel1@gmail.com>
2023-02-08 08:09:35 +0100

    3: 0x0B task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,7 +13,7 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        self.firts_name = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
