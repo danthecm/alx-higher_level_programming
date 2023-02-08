5abae7a89991b63cf98ac1889ecd254cf8f8aa72
danthecm <etchiedaniel1@gmail.com>
2023-02-08 07:22:29 +0100

    1: 0x0B task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that creates a list from command line arguments
+and saves it to a json file
+"""
+
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+args_list = sys.argv[1:]
+
+
+with open("add_item.json", "a+") as file:
+    loaded_file = []
+    try:
+        loaded_file = load_from_json_file("add_item.json")
+    except:
+        pass
+    for item in args_list:
+        loaded_file.append(item)
+    save_to_json_file(loaded_file, "add_item.json")
