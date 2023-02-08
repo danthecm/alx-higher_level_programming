871e0f5b2dfa26a56b8d845bd80286e03eb4d2c0
danthecm <etchiedaniel1@gmail.com>
2023-02-08 08:22:51 +0100

    1: 0x0B task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A module containing a student class
+with first name, last name and age
+"""
+
+
+class Student:
+    """
+    A student class that is instantiated with
+
+    first name, last name and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Converts the object to a dictionary
+        """
+        my_dict = vars(self)
+        if isinstance(attrs, list):
+            return {item: my_dict[item] for item in my_dict if item in attrs}
+        return my_dict
+
+    def reload_from_json(self, json):
+        """
+        Reset the instance attributes based on the json given
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
