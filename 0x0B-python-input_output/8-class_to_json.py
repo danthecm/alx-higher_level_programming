69072f262877058944117e4f21fd40e6429a1996
danthecm <etchiedaniel1@gmail.com>
2023-02-08 07:41:24 +0100

    1: 0x0B task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that contains a function to convert 
+objects to dictionaries
+"""
+
+
+def class_to_json(obj):
+    """
+    Converts a class object to a dictionary
+    """
+    return vars(obj)
