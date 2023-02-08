13d05edbf2409f089f636fd7ec2d049e250b3a16
danthecm <etchiedaniel1@gmail.com>
2023-02-08 08:07:37 +0100

    1: 0x0B task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A module containing a student class
+with first name, last name and age
+"""
+
+
+class Student:
+    """
+    A student class that is instantiated with
+
+    first name, last name and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.firts_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Converts the object to a dictionary
+        """
+        return vars(self)
