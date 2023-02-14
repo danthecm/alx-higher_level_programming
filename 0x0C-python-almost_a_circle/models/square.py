d500bff662f8004eb5efea1298e3280f384bf718
danthecm <etchiedaniel1@gmail.com>
2023-02-14 23:30:18 +0100

    4: 0x0C task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         message = "[Square] ({}) {}/{} - {}"
