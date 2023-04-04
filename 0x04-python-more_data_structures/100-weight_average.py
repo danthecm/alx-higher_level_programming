83038cafb1b172f35d6257b051028d7092f06b58
cm <etchiedaniel1@gmail.com>
2023-04-04 14:03:45 +0100

    1: 0x04 task 13: created weight_average func

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    numerator = 0
+    denominator = 0
+
+    for item in my_list:
+        numerator += (item[0] * item[1])
+        denominator += item[1]
+
+    return numerator / denominator
