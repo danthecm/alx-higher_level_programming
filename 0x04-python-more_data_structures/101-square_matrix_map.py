4493a78ac74115982070da592d4d1857af540a9b
cm <etchiedaniel1@gmail.com>
2023-04-04 20:10:51 +0100

    2: 0x04 task 14: fix: file must be 3 line checker rule

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(square_matrix, matrix))
-
-
-def square_matrix(matrix):
-    return list(map(square_list, matrix))
-
-
-def square_list(item):
-    return item * item
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
