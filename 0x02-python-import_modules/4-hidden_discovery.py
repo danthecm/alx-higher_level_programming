c45c01629347b2c0a0006f6228bd1f9fcf387dac
cm <etchiedaniel1@gmail.com>
2023-04-04 12:17:56 +0100

    6: 0x02 task 4: fix: pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     module_names = dir(hidden_4)
-    module_names = [name for name in module_names if name[0: 2] != "__" ]
+    module_names = [name for name in module_names if name[0: 2] != "__"]
     for name in module_names:
         print(name)
