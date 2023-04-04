d3c1cb519e11886f4956bbbc02eb7ce5bf8ffcd7
cm <etchiedaniel1@gmail.com>
2023-04-04 12:11:23 +0100

    4: 0x02 task 4: Fixing checker errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__": 
-    dir(hidden_4)
+    module_names = dir(hidden_4)
+    module_names = [name for name in module_names if name[0: 2] != "__" ]
+    for name in module_names:
+        print(name)
