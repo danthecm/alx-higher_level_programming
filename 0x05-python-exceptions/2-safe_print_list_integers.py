bb4bec1a83f842e1727087d03f709c58d9e47535
cm <etchiedaniel1@gmail.com>
2023-04-04 20:25:30 +0100

    3: 0x05 task 2: fix: fix error not raised for x greater than length of my_list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     real_number = 0
-    try:
-        for i in range(0, x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                real_number += 1
-            except ValueError as e:
+    for i in range(0, x):
+        try:
+            if type(my_list[i]) != int:
                 continue
-            except Exception as e:
-                raise e
-    except Exception as e:
-        raise e
-    finally:
-        print("\n", end="")
-        return real_number
+            print("{:d}".format(my_list[i]), end="")
+            real_number += 1
+        except Exception as e:
+            raise e
+    print("\n", end="")
+    return real_number
