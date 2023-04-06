fc2051e791877d51c44a14f0fafe7d4ab7974672
danthecm <etchiedaniel1@gmail.com>
2023-04-06 02:34:15 +0100

    2: 0x05 task 9: fix if condition checking

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     result = 0
 
     for i in range(1, 3):
-        if i > a:
+        if a > i:
             raise Exception("Too far")
         else:
             result += a ** b / i
