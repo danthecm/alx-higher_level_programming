838c6e1562b4ea6efede78b2ff8633d817eb9e2c
danthecm <etchiedaniel1@gmail.com>
2023-04-07 11:48:21 +0100

    2: 0x06 task 7: fix: fix no doc for module and methods

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
+"""
+A simple module implementing a node class and a linked list 
+"""
+
+
 class Node:
+    """A node class for singly linked lists"""
+
     def __init__(self, data, next_node=None):
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
@@ -30,12 +38,17 @@ class Node:
 
 
 class SinglyLinkedList:
+    """A singly linked list data structure"""
+
     def __init__(self):
         self.head = None
 
     def __str__(self) -> str:
         value_arr = []
-        traverse(self.head, value_arr)
+        temp_head = self.head
+        while temp_head.next_node is not None:
+            value_arr.append(temp_head.data)
+            temp_head = temp_head.next_node
         value_arr.sort()
         return "".join(
             map(lambda x: str(x) + "\n"
@@ -43,6 +56,7 @@ class SinglyLinkedList:
                 else str(x), value_arr))
 
     def sorted_insert(self, value):
+        """Insert a new node into the linked list"""
         if self.head is None:
             self.head = Node(value)
         else:
@@ -50,10 +64,23 @@ class SinglyLinkedList:
             temp = self.head
             new_node.next_node = temp
             self.head = new_node
+        # self.head = traverse(self.head)
 
 
-def traverse(head, arr):
+def traverse(head, arr=[], new_head=None):
+    """Traverse through the linked list and sort the elements
+    Returns:
+        A new linked list head
+    """
     arr.append(head.data)
     if head.next_node is None:
-        return ""
+        for item in arr:
+            if new_head is None:
+                new_head = Node(item)
+            else:
+                new_node = Node(item)
+                temp = new_head
+                new_node.next_node = temp
+                new_head = new_node
+        return new_head
     return traverse(head.next_node, arr)
