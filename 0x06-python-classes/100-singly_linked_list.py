a470f6e2dd15c01ca90e692f7b494a9d1bd1d2f9
danthecm <etchiedaniel1@gmail.com>
2023-04-07 08:47:10 +0100

    1: 0x06 task 6: feat: created singlyList class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -27,3 +27,30 @@ class Node:
         if value and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self) -> str:
+        return self.__head
+
+    def sorted_insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            new_node = Node(value)
+            temp = self.head
+            new_node.next_node = temp
+            self.head = new_node
+
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(10)
+
+print(sll.traverse(sll.head))
+
+# print(sll.head.data)
