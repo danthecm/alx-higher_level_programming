5350e4015ddac87640050aca5dd4527376635608
danthecm <etchiedaniel1@gmail.com>
2023-04-07 11:55:06 +0100

    4: 0x06 task 6: fix: remove test codes from the module

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -84,18 +84,3 @@ def traverse(head, arr=[], new_head=None):
                 new_head = new_node
         return new_head
     return traverse(head.next_node, arr)
-
-
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)
