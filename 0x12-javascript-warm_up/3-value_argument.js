b3b42425962ad0a55d2c99f5dc5a99babf3997e5
danthecm <etchiedaniel1@gmail.com>
2023-04-12 02:24:26 +0100

    2: 0x12 task 2: fix argv index

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[3]) {
-  console.log(process.argv[3]);
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-  console.log('No argument');
+  console.log("No argument");
 }
