726497932eef11412c806c1e6c4b826ac083c6c3
danthecm <etchiedaniel1@gmail.com>
2023-04-14 06:38:05 +0100

    2: 0x13 task 6: fix checker errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const MySquare = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends MySquare {
   constructor (size) {
     super(size, size);
   }
