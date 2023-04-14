61f33af894d2965399c21430fcbb0185e6114910
danthecm <etchiedaniel1@gmail.com>
2023-04-14 06:02:24 +0100

    1: 0x13 task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let rep = "";
+      for (let j = 0; j < this.width; j++) {
+        rep += "X";
+      }
+      console.log(rep);
+    }
+  }
+}
+module.exports = Rectangle;
