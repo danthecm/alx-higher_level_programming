f7f7bf3826660764b3babbbdd3b30dc9c3e06bcd
danthecm <etchiedaniel1@gmail.com>
2023-04-14 06:10:56 +0100

    1: 0x13 task 4: initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rep = '';
+      for (let j = 0; j < this.width; j++) {
+        rep += 'X';
+      }
+      console.log(rep);
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+module.exports = Rectangle;
