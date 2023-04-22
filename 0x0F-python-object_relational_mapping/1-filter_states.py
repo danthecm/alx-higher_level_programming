6ad1cf42b418b2d2523d40fe45c7addf0ca68a5f
danthecm <etchiedaniel1@gmail.com>
2023-04-22 10:07:24 +0100

    1: 0x0F task 1: starts with

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A module that connects to a mysql database and selects
+all states in the state table
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=db_name)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
