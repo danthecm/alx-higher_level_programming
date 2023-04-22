5a6af813720e7f12af514f111f990664df646a52
danthecm <etchiedaniel1@gmail.com>
2023-04-22 21:45:23 +0100

    1: 0x0F task 11: Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects
+that contains the letter a from the a database
+"""
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state = State()
+
+    new_state.name = "Louisiana"
+
+    session.commit(new_state)
+
+    session.close()
