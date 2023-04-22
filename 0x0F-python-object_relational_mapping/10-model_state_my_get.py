051c465b495d4d0c49d20c6be7d4c68cd0bdc922
danthecm <etchiedaniel1@gmail.com>
2023-04-22 21:40:54 +0100

    1: 0x0F task 10: Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects
+that contains the letter a from the a database
+"""
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(username, password, db_name),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    query = session.query(State).filter(
+        State.name == state_name).order_by(State.id)
+
+    if query:
+        for state in query:
+            print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()
