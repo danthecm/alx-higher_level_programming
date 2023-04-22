48e395d7dfb01b07a90dd961fb88ebd1875897ab
danthecm <etchiedaniel1@gmail.com>
2023-04-22 22:53:55 +0100

    3: 0x0F task 5: fixing checker errors

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,27 +3,17 @@
 A module that connects to a mysql database and selects
 all states where name matches the 4th cmd arg
 """
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    search_str = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", user=username,
-                         passwd=password, db=db_name)
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-
-    query = "SELECT cities.name FROM cities JOIN states \
-        ON cities.state_id = states.id WHERE states.name \
-            LIKE BINARY %s ORDER BY cities.id ASC"
-
-    cur.execute(query, (search_str + "%",))
-
-    result = cur.fetchall()
-
-    for i in range(len(result)):
-        print(result[i][0], end=", " if i != len(result) - 1 else "\n")
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name = %s \
+    ORDER BY cities.id ASC", (argv[4],))
+    rows = cur.fetchall()
+    print(", ".join(city[0] for city in rows))
+    cur.close()
+    db.close()
