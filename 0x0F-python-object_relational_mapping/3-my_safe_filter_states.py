5811ffd0df8acff314d190e2b2c5073932674f0a
danthecm <etchiedaniel1@gmail.com>
2023-04-22 10:28:35 +0100

    1: 0x0F task 3: SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A module that connects to a mysql database and selects
+all states where name matches the 4th cmd arg
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    search_str = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=db_name)
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    
+    cur.execute(query, (search_str + "%",))
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
