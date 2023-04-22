574cd3a6c7aa610490fc88790e4a433b2c5019b7
danthecm <etchiedaniel1@gmail.com>
2023-04-22 09:59:54 +0100

    2: 0x0F task 0: meet all task req

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,19 +6,21 @@ all states in the state table
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=username,
+                        passwd=password, db=db_name)
 
-db = MySQLdb.connect(host="localhost", user=username,
-                     passwd=password, db=db_name)
+    cur = db.cursor()
 
-cur = db.cursor()
+    cur.execute("SELECT * FROM states")
 
-cur.execute("SELECT * FROM states")
+    result = list(cur.fetchall())
 
-result = cur.fetchall()
+    result.sort(key=lambda x: x[0])
 
-for row in result:
-    print(row)
+    for row in result:
+        print(row)
