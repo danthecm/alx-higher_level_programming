ff55a402a31d75c99c5bc94c2e47cca07ea07c3c
danthecm <etchiedaniel1@gmail.com>
2023-04-22 11:43:30 +0100

    2: 0x0F task 5: All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A module that connects to a mysql database and selects
-all states in the state table
+all states where name matches the 4th cmd arg
 """
 import sys
 import MySQLdb
@@ -10,16 +10,20 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+    search_str = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", user=username,
                          passwd=password, db=db_name)
 
     cur = db.cursor()
 
-    cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY id ASC")
+    query = "SELECT cities.name FROM cities JOIN states \
+        ON cities.state_id = states.id WHERE states.name \
+            LIKE BINARY %s ORDER BY cities.id ASC"
+
+    cur.execute(query, (search_str + "%",))
 
     result = cur.fetchall()
 
-    for row in result:
-        print(row)
+    for i in range(len(result)):
+        print(result[i][0], end=", " if i != len(result) - 1 else "\n")
