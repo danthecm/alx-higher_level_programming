98918307be1258210dbe60359d999d66ce9ddf92
danthecm <etchiedaniel1@gmail.com>
2023-04-28 00:57:04 +0100

    5: 0x10 task 6: find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,7 +12,20 @@ def find_peak(list_of_integers):
     Returns:
     int: peak if list is not empty else None
     """
-    if len(list_of_integers) > 0:
-        list_of_integers.sort()
-        return list_of_integers[-1]
-    return None
+    if list_of_integers == []: 
+         return None 
+  
+    size = len(list_of_integers) 
+    if size == 1: 
+         return list_of_integers[0] 
+    elif size == 2: 
+         return max(list_of_integers) 
+  
+    mid = int(size / 2) 
+    peak = list_of_integers[mid] 
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+          return peak 
+    elif peak < list_of_integers[mid - 1]: 
+        return find_peak(list_of_integers[:mid]) 
+    else: 
+        return find_peak(list_of_integers[mid + 1:])
