d08dd82f1e99c67a36f0a82664881ab2c2c9e96b
danthecm <etchiedaniel1@gmail.com>
2023-04-28 21:45:47 +0100

    3: 0x11 task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,6 @@ A python module that makes a request to the url passed as argument
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
     header = response.getheader('X-Request-Id')
     print(header)
